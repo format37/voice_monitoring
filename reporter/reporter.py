58d8f89f369ec2bed9dc43e2dd346b56b70fb8e3
alex <format37@gmail.com>
2021-12-06 09:07:03 +0300

    new file:   reporter/reporter.py

---
+++ reporter/reporter.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import pymssql
+import os
+import time
+import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
+import requests
+import numpy as np
+
+
+def connect_sql():
+
+	return
